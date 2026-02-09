23eec7edbc931b6487e6f0c0bf0d48786828e5b4
guigermin <guilhermecarlosdeoliveira00@gmail.com>
2026-02-09 09:42:50 +0100

    Añade métodos multiplicar y dividir

--- calculadora03/calculadora03.java
+++ calculadora03/calculadora03.java
@@ -7,4 +7,11 @@ public class calculadora03 {
 	public int restar(int a, int b) {
 		return a - b;
 	}
+	 public int multiplicar(int a, int b) {
+	    return a * b;
+	}
+
+	 public double dividir(int a, int b) {
+	    return (double) a / b;
+	}
 }
